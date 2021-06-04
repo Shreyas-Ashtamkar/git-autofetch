9357f5ae3c87f486ecca74e2d80747dceff3f560
Shreyas Ashtamkar <Shreyu@programmer.net>
2021-06-04 15:44:14 +0530

    changes

--- install.py
+++ install.py
@@ -7,22 +7,18 @@ if sys.version_info.major != 3:
 PYTHON_EXEC  = sys.executable
 THIS_FOLDER  = os.path.dirname(__file__)
 
-PROJECTS_LIST = './projectlist.py'
+def create_file(file_name, text, replace=False):
+    if not replace and os.path.isfile(f"{THIS_FOLDER}/{file_name}"):
+        print(f"{file_name.replace('.',' ')} exists")
+    else:
+        print(f"creating {file_name.replace('.',' ')} file")
+        with open(file_name, 'w') as file:
+            file.write(text)
 
-if not os.path.isfile(PROJECTS_LIST):
-    xmlfile = open(PROJECTS_LIST, 'w') 
-    xmlfile.write(\
-        'from projects import _Project\n\n' \
-        '# Create A Project Object for each and every project you want to auto fetch\n'\
-        'PROJECT_LIST = [\n'\
-        '    _Project(\n'\
-        f'        path="{THIS_FOLDER}"\n'\
-        '    ),\n' \
-        ']\n'
-    )
-    xmlfile.close()
-
-service_script = \
+create_file(
+file_name="autofetch.service", 
+replace=True,
+text=\
 f'''[Unit]
 Description=Github Projects Auto Fetcher
 After=multi-user.target
@@ -36,6 +32,30 @@ ExecStart={PYTHON_EXEC} {THIS_FOLDER if THIS_FOLDER else '.' }/autofetch.py
 [Install]
 WantedBy=multi-user.target
 '''
+)
 
-with open("autofetch.service",'w') as SERVICE_FILE:
-    SERVICE_FILE. write(service_script)
+create_file(
+file_name="projectlist.py", 
+replace=False,
+text= \
+f'''
+from projects import _Project
+
+# Create A Project Object for each and every project you want to auto fetch
+PROJECT_LIST = [
+    _Project(
+        path="{THIS_FOLDER}"
+    ),
+]
+'''
+)
+
+create_file(
+file_name="configs.py",
+replace=False,
+text=\
+f'''
+DELAY = 30
+
+'''
+)
