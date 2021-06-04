cbf7de3f4d135ab8f035e3af0cf19307e3f7dfb2
Shreyas Ashtamkar <Shreyu@programmer.net>
2021-06-04 13:23:49 +0530

    installer changed

--- install.py
+++ install.py
@@ -4,22 +4,29 @@ if sys.version_info.major != 3:
     print("Python version 3 required to run this script")
     exit(1)
 
-service_file = open("projectfetcher.service",'w')
+PYTHON_EXEC  = sys.executable
+THIS_FOLDER  = os.path.dirname(__file__)
 
-def println(text=""):
-    service_file.write(text+'\n')
+PROJECTS_XML = './project_list.xml'
 
-# Writing Unit part of Unit
-println('[Unit]')
-println("Description=Github Projects Auto Fetcher")
-println("After=multi-user.target")
-println()
-# Writing the actual Service
-println("[Service]")
-println("Type=simple")
-println(f"ExecStart={sys.executable} {os.path.dirname(__file__)}/projectfetcher.py")
-println()
-# Writing the actual Install
-println("[Install]")
-println("WantedBy=multi-user.target")
+if not os.path.isfile(PROJECTS_XML):
+    xmlfile = open(PROJECTS_XML,)    
+    xmlfile.write('')
 
+service_script = \
+f'''[Unit]
+Description=Github Projects Auto Fetcher
+After=multi-user.target
+# Conflicts=getty@tty1.service
+
+[Service]
+Type=simple
+ExecStart={PYTHON_EXEC} {THIS_FOLDER if THIS_FOLDER else '.' }/autofetch.py
+# StandardInput=tty-force
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+with open("autofetch.service",'w') as SERVICE_FILE:
+    SERVICE_FILE. write(service_script)
